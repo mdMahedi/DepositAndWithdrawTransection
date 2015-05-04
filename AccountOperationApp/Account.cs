06c2acf736e1ce00126dac77a0d7f5930a930b64
mdMahedi <md1mahedihasan@gmail.com>
2015-05-04 21:51:59 +0600

    init commit

---
+++ AccountOperationApp/Account.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountOperationApp
+{
+    //class Account
+    //{
+    //    public string accountNumber;
+    //    public string customerName;
+    //    public double amount=0;
+
+    //    public double AddAmount(double dipositAmount)
+    //    {
+    //        return amount+dipositAmount;
+    //    }
+    //    public double SubtractAmount(double dipositAmount)
+    //    {
+    //        return amount - dipositAmount;
+    //    }
+    //}
+
+    public class Account
+    {
+
+        public string id { get; set; }
+
+        public string name { get; set; }
+
+        public Decimal balance { get; set; }
+
+        public Decimal DepositMoney(Decimal amount)
+        {
+            balance += amount;
+            return balance;
+        }
+
+        public Decimal WithdrawMoney(Decimal amount)
+        {
+        Decimal moneyAfterWithdrawal = balance-amount;
+
+        if(moneyAfterWithdrawal >= 0)
+        {
+           balance = moneyAfterWithdrawal;
+           return balance;
+        }
+            return amount;
+        }
+    }
+}
---
+++ AccountOperationApp/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountOperationApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Account acc=new Account();
+        private void createButton_Click(object sender, EventArgs e)
+        {
+            acc.id = accountNumberTextBox.Text;
+            acc.name = customerNameTextBox.Text;
+            accountNumberTextBox.Clear();
+            customerNameTextBox.Clear();
+        }
+
+        private void depositButton_Click(object sender, EventArgs e)
+        {
+            decimal amount = Convert.ToDecimal(amountTextBox.Text);
+            acc.DepositMoney(amount);
+            amountTextBox.Clear();
+        }
+
+        private void withdrawButton_Click(object sender, EventArgs e)
+        {
+
+            decimal amount = Convert.ToDecimal(amountTextBox.Text);
+            if (acc.balance < amount)
+            {
+                MessageBox.Show("Not enough balance");
+            }
+            else
+            {
+                acc.WithdrawMoney(amount);
+                amountTextBox.Clear();
+            }
+        }
+        private void reportButton_Click(object sender, EventArgs e)
+        {
+            string id = acc.id;
+            string name = acc.name;
+            decimal amount = acc.balance;
+            
+            MessageBox.Show(name + ", Your Account Number is : " + id + " and it's balance : " + amount);
+        }
+    }
+}
